37a3b603471c632b6e9f4a672b64d02789ea7772
bowfun <bowfun@bownet.xyz>
2023-11-28 21:51:02 -0700

    Add permission check Now only allowed users can start a game, except it doesn't work. Oh yea, and readme improvements.

--- commands/StartTrivia/start-trivia.js
+++ commands/StartTrivia/start-trivia.js
@@ -16,19 +16,26 @@ module.exports = {
 			option
 				.setName('max-winners')
 				.setRequired(true)
-				.setDescription('The maximum amount of winners in a game (after which the game ends).')),
+				.setDescription('The maximum amount of winners fro a question (after which the trivia game ends).')),
 	async execute(interaction) {
 		const rawData = fs.readFileSync('./config.json');
+		const allowedRoles = JSON.parse(rawData).adminRoleIds;
 		const jsonData = JSON.parse(rawData).trivia;
 
 		const triviaID = interaction.options.getInteger('trivia-id');
 		const maxWinners = interaction.options.getInteger('max-winners');
 
+		const hasRoles = allowedRoles.some((roleID) => interaction.member.roles.cache.has(roleID));
+		if (!hasRoles) {
+			interaction.reply({ content: 'You do not have permission to use this command!', ephemeral: true });
+			return;
+		}
+
 		for (const question of jsonData) {
 			if (triviaID === question.id) {
 				endQuiz();
 				if (maxWinners <= 0) {
-					interaction.reply({ content: 'Max winners must be 1 or more.', ephemeral: true });
+					interaction.reply({ content: 'Maximum winners must be 1 or more.', ephemeral: true });
 					return;
 				}
 				startQuiz(question.id, maxWinners);
